d82605d57024217907584449578f17c3142429b8
Damian Ball <projects@dev.damianball.com>
2012-01-29 17:15:51 -0800

    updated: ddl

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `zombiesource` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
-USE `zombiesource`;
+CREATE DATABASE  IF NOT EXISTS `zsource_dev` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `zsource_dev`;
 -- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
 --
--- Host: localhost    Database: zombiesource
+-- Host: localhost    Database: zsource_dev
 -- ------------------------------------------------------
--- Server version	5.1.37-1ubuntu5.5
+-- Server version	5.5.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,35 @@ USE `zombiesource`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `feed`
+--
+
+DROP TABLE IF EXISTS `feed`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `feed` (
+  `id` varchar(36) COLLATE utf8_bin NOT NULL,
+  `zombieid` varchar(36) COLLATE utf8_bin NOT NULL,
+  `tagid` varchar(36) COLLATE utf8_bin NOT NULL,
+  `invalid` int(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `feed_player` (`zombieid`),
+  KEY `feed_tag` (`tagid`),
+  CONSTRAINT `feed_player` FOREIGN KEY (`zombieid`) REFERENCES `player` (`id`),
+  CONSTRAINT `feed_tag` FOREIGN KEY (`tagid`) REFERENCES `tag` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feed`
+--
+
+LOCK TABLES `feed` WRITE;
+/*!40000 ALTER TABLE `feed` DISABLE KEYS */;
+/*!40000 ALTER TABLE `feed` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `game_state`
@@ -65,7 +94,7 @@ CREATE TABLE `users` (
   `created` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=158 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -78,34 +107,29 @@ LOCK TABLES `users` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `player`
+-- Table structure for table `ci_sessions`
 --
 
-DROP TABLE IF EXISTS `player`;
+DROP TABLE IF EXISTS `ci_sessions`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `player` (
-  `id` varchar(36) COLLATE utf8_bin NOT NULL,
-  `userid` int(11) NOT NULL,
-  `gameid` varchar(36) COLLATE utf8_bin NOT NULL,
-  `datecreated` datetime NOT NULL,
-  `original_zombie` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `user_game_unique` (`userid`,`gameid`) USING BTREE,
-  KEY `player_game` (`gameid`),
-  KEY `player_users` (`userid`) USING BTREE,
-  CONSTRAINT `player_game` FOREIGN KEY (`gameid`) REFERENCES `game` (`id`),
-  CONSTRAINT `player_users` FOREIGN KEY (`userid`) REFERENCES `users` (`id`)
+CREATE TABLE `ci_sessions` (
+  `session_id` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `ip_address` varchar(16) COLLATE utf8_bin NOT NULL DEFAULT '0',
+  `user_agent` varchar(150) COLLATE utf8_bin NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `player`
+-- Dumping data for table `ci_sessions`
 --
 
-LOCK TABLES `player` WRITE;
-/*!40000 ALTER TABLE `player` DISABLE KEYS */;
-/*!40000 ALTER TABLE `player` ENABLE KEYS */;
+LOCK TABLES `ci_sessions` WRITE;
+/*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -121,7 +145,7 @@ CREATE TABLE `user_data` (
   `timestamp` datetime NOT NULL,
   `value` varchar(1024) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`userid`,`name`,`timestamp`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -131,31 +155,6 @@ CREATE TABLE `user_data` (
 LOCK TABLES `user_data` WRITE;
 /*!40000 ALTER TABLE `user_data` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_data` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `login_attempts`
---
-
-DROP TABLE IF EXISTS `login_attempts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `login_attempts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ip_address` varchar(40) COLLATE utf8_bin NOT NULL,
-  `login` varchar(50) COLLATE utf8_bin NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `login_attempts`
---
-
-LOCK TABLES `login_attempts` WRITE;
-/*!40000 ALTER TABLE `login_attempts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `login_attempts` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -199,8 +198,8 @@ CREATE TABLE `game` (
   PRIMARY KEY (`id`),
   KEY `game_game_state` (`game_stateid`),
   KEY `game_timezone` (`timezoneid`),
-  CONSTRAINT `game_timezone` FOREIGN KEY (`timezoneid`) REFERENCES `timezone` (`id`),
-  CONSTRAINT `game_game_state` FOREIGN KEY (`game_stateid`) REFERENCES `game_state` (`id`)
+  CONSTRAINT `game_game_state` FOREIGN KEY (`game_stateid`) REFERENCES `game_state` (`id`),
+  CONSTRAINT `game_timezone` FOREIGN KEY (`timezoneid`) REFERENCES `timezone` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -215,55 +214,36 @@ INSERT INTO `game` VALUES ('9a051bbc-3ebc-11e1-b778-000c295b88cf','Humans vs Zom
 UNLOCK TABLES;
 
 --
--- Table structure for table `game_setting`
+-- Table structure for table `tag`
 --
 
-DROP TABLE IF EXISTS `game_setting`;
+DROP TABLE IF EXISTS `tag`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `game_setting` (
-  `gameid` varchar(36) COLLATE utf8_bin NOT NULL,
-  `name` varchar(128) COLLATE utf8_bin NOT NULL,
-  `value` varchar(1024) COLLATE utf8_bin DEFAULT NULL,
-  `editable_state_list` varchar(512) COLLATE utf8_bin DEFAULT NULL,
-  PRIMARY KEY (`gameid`,`name`),
-  CONSTRAINT `game_setting_game` FOREIGN KEY (`gameid`) REFERENCES `game` (`id`)
+CREATE TABLE `tag` (
+  `id` varchar(36) COLLATE utf8_bin NOT NULL,
+  `taggerid` varchar(36) COLLATE utf8_bin NOT NULL,
+  `taggeeid` varchar(36) COLLATE utf8_bin NOT NULL,
+  `datetimeclaimed` datetime NOT NULL,
+  `datetimecreated` datetime NOT NULL,
+  `longitude` decimal(7,5) DEFAULT NULL,
+  `latitude` decimal(7,5) DEFAULT NULL,
+  `invalid` int(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `tag_player_tagger` (`taggerid`),
+  KEY `tag_player_taggee` (`taggeeid`),
+  CONSTRAINT `tag_player_taggee` FOREIGN KEY (`taggeeid`) REFERENCES `player` (`id`),
+  CONSTRAINT `tag_player_tagger` FOREIGN KEY (`taggerid`) REFERENCES `player` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `game_setting`
+-- Dumping data for table `tag`
 --
 
-LOCK TABLES `game_setting` WRITE;
-/*!40000 ALTER TABLE `game_setting` DISABLE KEYS */;
-/*!40000 ALTER TABLE `game_setting` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `ci_sessions`
---
-
-DROP TABLE IF EXISTS `ci_sessions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ci_sessions` (
-  `session_id` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '0',
-  `ip_address` varchar(16) COLLATE utf8_bin NOT NULL DEFAULT '0',
-  `user_agent` varchar(150) COLLATE utf8_bin NOT NULL,
-  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`session_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `ci_sessions`
---
-
-LOCK TABLES `ci_sessions` WRITE;
-/*!40000 ALTER TABLE `ci_sessions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `ci_sessions` ENABLE KEYS */;
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -341,6 +321,152 @@ LOCK TABLES `user_profiles` WRITE;
 /*!40000 ALTER TABLE `user_profiles` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user_profiles` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `player`
+--
+
+DROP TABLE IF EXISTS `player`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `player` (
+  `id` varchar(36) COLLATE utf8_bin NOT NULL,
+  `userid` int(11) NOT NULL,
+  `gameid` varchar(36) COLLATE utf8_bin NOT NULL,
+  `datecreated` datetime NOT NULL,
+  `original_zombie` tinyint(4) DEFAULT NULL,
+  `dateremoved` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_game_unique` (`userid`,`gameid`),
+  KEY `player_game` (`gameid`),
+  KEY `player_users` (`userid`) USING BTREE,
+  CONSTRAINT `player_game` FOREIGN KEY (`gameid`) REFERENCES `game` (`id`),
+  CONSTRAINT `player_users` FOREIGN KEY (`userid`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `player`
+--
+
+LOCK TABLES `player` WRITE;
+/*!40000 ALTER TABLE `player` DISABLE KEYS */;
+/*!40000 ALTER TABLE `player` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `player_to_team`
+--
+
+DROP TABLE IF EXISTS `player_to_team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `player_to_team` (
+  `playerid` varchar(36) COLLATE utf8_bin NOT NULL,
+  `teamid` varchar(36) COLLATE utf8_bin NOT NULL,
+  `datecreated` datetime NOT NULL,
+  `dateremoved` datetime DEFAULT NULL,
+  PRIMARY KEY (`playerid`,`teamid`,`datecreated`),
+  KEY `player_to_team_team` (`teamid`),
+  KEY `player_to_team_player` (`playerid`),
+  CONSTRAINT `player_to_team_player` FOREIGN KEY (`playerid`) REFERENCES `player` (`id`),
+  CONSTRAINT `player_to_team_team` FOREIGN KEY (`teamid`) REFERENCES `team` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `player_to_team`
+--
+
+LOCK TABLES `player_to_team` WRITE;
+/*!40000 ALTER TABLE `player_to_team` DISABLE KEYS */;
+/*!40000 ALTER TABLE `player_to_team` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login_attempts`
+--
+
+DROP TABLE IF EXISTS `login_attempts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login_attempts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(40) COLLATE utf8_bin NOT NULL,
+  `login` varchar(50) COLLATE utf8_bin NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login_attempts`
+--
+
+LOCK TABLES `login_attempts` WRITE;
+/*!40000 ALTER TABLE `login_attempts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `login_attempts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `game_setting`
+--
+
+DROP TABLE IF EXISTS `game_setting`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `game_setting` (
+  `gameid` varchar(36) COLLATE utf8_bin NOT NULL,
+  `name` varchar(128) COLLATE utf8_bin NOT NULL,
+  `value` varchar(1024) COLLATE utf8_bin DEFAULT NULL,
+  `editable_state_list` varchar(512) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`gameid`,`name`),
+  CONSTRAINT `game_setting_game` FOREIGN KEY (`gameid`) REFERENCES `game` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `game_setting`
+--
+
+LOCK TABLES `game_setting` WRITE;
+/*!40000 ALTER TABLE `game_setting` DISABLE KEYS */;
+/*!40000 ALTER TABLE `game_setting` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `team`
+--
+
+DROP TABLE IF EXISTS `team`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `team` (
+  `id` varchar(36) COLLATE utf8_bin NOT NULL,
+  `gameid` varchar(36) COLLATE utf8_bin NOT NULL,
+  `name` varchar(100) COLLATE utf8_bin NOT NULL,
+  `datecreated` datetime NOT NULL,
+  `private` int(1) DEFAULT NULL,
+  `regcode` varchar(10) COLLATE utf8_bin DEFAULT NULL,
+  `gravatar_email` varchar(128) COLLATE utf8_bin DEFAULT NULL,
+  `description` text COLLATE utf8_bin,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `gameid_name_UNIQUE` (`gameid`,`name`) USING BTREE,
+  UNIQUE KEY `regcode_UNIQUE` (`regcode`),
+  KEY `team_game` (`gameid`),
+  CONSTRAINT `team_game` FOREIGN KEY (`gameid`) REFERENCES `game` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `team`
+--
+
+LOCK TABLES `team` WRITE;
+/*!40000 ALTER TABLE `team` DISABLE KEYS */;
+INSERT INTO `team` VALUES ('4b837e2e-4561-11e1-b364-080027008458','9a051bbc-3ebc-11e1-b778-000c295b88cf','The Killers','0000-00-00 00:00:00',0,NULL,NULL,NULL),('639425b7-4564-11e1-b364-080027008458','9a051bbc-3ebc-11e1-b778-000c295b88cf','The Who','0000-00-00 00:00:00',0,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `team` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -351,4 +477,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-01-14 16:01:23
+-- Dump completed on 2012-01-29 17:14:46
