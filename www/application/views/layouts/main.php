f2b9ba25041538044c3d771293850b06d0777cb4
Mike Solomon <solo4857@vandals.uidaho.edu>
2012-08-18 14:43:08 -0700

    IE fixes (FML)

--- www/application/views/layouts/main.php
+++ www/application/views/layouts/main.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="no-js">
 <head>
   <meta charset="utf-8">
   <title>Braaaaains</title>
 
    <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/bootswatch.css">
 
+  <script type="text/javascript" src="<?php echo base_url('js/modernizr.custom.05649.min.js'); ?>"></script>
   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-  <script type="text/javascript" src= <?php echo base_url("js/highcharts/highcharts.js") ?> ></script>
+  <script type="text/javascript" src="<?php echo base_url('js/highcharts/highcharts.js'); ?>"></script>
+  <script type="text/javascript" src="<?php echo base_url('js/fix_placeholder.js'); ?>"></script>
 
   <script type="text/javascript" src="<?php echo base_url();?>js/jquery.tablesorter.js"></script>
 
