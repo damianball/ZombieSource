3b35ddc83b9c425814a35175fe1b053fb582373f
Mike Solomon <solo4857@vandals.uidaho.edu>
2012-08-26 23:34:24 -0700

    Added test account for tweeting (including private API key!)

--- www/application/config/organization.php
+++ www/application/config/organization.php
@@ -9,7 +9,12 @@ $config['tumblr_info_tag'] = 'info';
 $config['tumblr_api_key'] = "QTCRcblt2a8XOalgBp2F7KIpZUqjtrHUIkMmDVMAgj1PvgCUJv";
 // The number of Tumblr announcements (posts) to display
 $config['tumblr_num_posts'] = 1;
+
+//// Twitter API information (for sending tweets, not listing them; user-based)
+// @TODO: delete the secret token and replace (NOT in git!) with another
+$config['twitter_oauth_token'] = '783583686-HLIGmYVBW1zH6njX17ASoG7LJDhUzlbEf6tTbAx8';
+$config['twitter_oauth_token_secret'] = 'PibQhSxAYlOHkfiQ71Gqf9iSmkjG3pIAxGZ7cm7j68';
 // The Twitter hashtag for people to get their tweets posted
 $config['twitter_hashtag'] = '#VandalsHvZ';
 // The search string for the Twitter feed
-$config['twitter_search'] = json_encode('from:cba OR #fml OR ' . $config['twitter_hashtag']);
+$config['twitter_search'] = json_encode('from:placedinbags OR #fml OR ' . $config['twitter_hashtag']);
