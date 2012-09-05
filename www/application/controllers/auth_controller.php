98ea4dd977039230b73ca962a45e5e57664d408c
Damian Ball <projects@dev.damianball.com>
2012-09-05 02:14:33 -0700

    fixed: formatting of auth_controller added: new receive array payload function for batch adding key/value pairs

--- www/application/controllers/auth_controller.php
+++ www/application/controllers/auth_controller.php
@@ -2,106 +2,105 @@
 
 class auth_controller extends CI_Controller
 {
-  function index()
-  {
-    if ($message = $this->session->flashdata('message')) {
-      $this->load->view('auth/general_message', array('message' => $message));
-    } else {
-      redirect('/auth/login/');
-    }
-  }
-
-  function registration_success(){
-    $layout_data['top_bar'] = $this->load->view('layouts/logged_out_topbar','', true);
-    $layout_data['content_body'] = $this->load->view('auth/registration_success','', true);
-    $layout_data['footer'] = $this->load->view('layouts/footer', '', true);          
-    $this->load->view('layouts/main', $layout_data);
-  }
-
-  function email_success(){
-    $layout_data['top_bar'] = $this->load->view('layouts/logged_out_topbar','', true);
-    $layout_data['content_body'] = $this->load->view('auth/email_conformation_success', '', true);
-    $layout_data['footer'] = $this->load->view('layouts/footer', '', true);          
-    $this->load->view('layouts/main', $layout_data);
-  }
-
-  function login_fail(){
-    $layout_data['top_bar'] = $this->load->view('layouts/logged_out_topbar','', true);
-    $layout_data['content_body'] = $this->load->view('auth/login_fail', '', true);
-    $layout_data['footer'] = $this->load->view('layouts/footer', '', true);          
-    $this->load->view('layouts/main', $layout_data);
-  }
-
-  function login()
-  {
-    if ($this->tank_auth->is_logged_in()) {                 // logged in
-      redirect('/home');
-    }elseif ($this->tank_auth->is_logged_in(FALSE)) {            // logged in, not activated
-      redirect('/auth/send_again/');
-    } else {
-      $data['login_by_username'] = ($this->config->item('login_by_username', 'tank_auth') AND
-        $this->config->item('use_username', 'tank_auth'));
-      $data['login_by_email'] = $this->config->item('login_by_email', 'tank_auth');
-
-      $this->form_validation->set_rules('login', 'Login', 'trim|required|xss_clean');
-      $this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
-      $this->form_validation->set_rules('remember', 'Remember me', 'integer');
-
-      // Get login for counting attempts to login
-      // if ($this->config->item('login_count_attempts', 'tank_auth') AND
-      //   ($login = $this->input->post('login'))) {
-      // $login = $this->security->xss_clean($login);
-      // } else {
-      //   //$login = '';
-      // }
-
-      $login = $this->input->post('login');
-      $login = $this->security->xss_clean($login);
-
-      $data['use_recaptcha'] = $this->config->item('use_recaptcha', 'tank_auth');
-      if ($this->tank_auth->is_max_login_attempts_exceeded($login)) {
-        if ($data['use_recaptcha']){
-          //$this->form_validation->set_rules('recaptcha_response_field', 'Confirmation Code', 'trim|xss_clean|required|callback__check_recaptcha');
+    function index()
+    {
+        if ($message = $this->session->flashdata('message')) {
+            $this->load->view('auth/general_message', array('message' => $message));
         } else {
-          //$this->form_validation->set_rules('captcha', 'Confirmation Code', 'trim|xss_clean|required|callback__check_captcha');
+            redirect('/auth/login/');
         }
-      }
-      $data['errors'] = array();
-
-      if ($this->form_validation->run()) {  // validation ok
-      if ($this->tank_auth->login(
-        $this->form_validation->set_value('login'),
-        $this->form_validation->set_value('password'),
-        $this->form_validation->set_value('remember'),
-        $data['login_by_username'],
-        $data['login_by_email'])) {               // success
-          redirect('profile');
-      } else {
-        $errors = $this->tank_auth->get_error_message();
-        if (isset($errors['banned'])) {               // banned user
-        $this->_show_message($this->lang->line('auth_message_banned').' '.$errors['banned']);
-
-        } elseif (isset($errors['not_activated'])) {        // not activated user
-        redirect('/auth/send_again/');
-
-        } else {        // fail
-        foreach ($errors as $k => $v) $data['errors'][$k] = $this->lang->line($v);
-        redirect('/auth/login_fail', $data);
-        }
-      }
-      }
-      $data['show_captcha'] = FALSE;
-      if ($this->tank_auth->is_max_login_attempts_exceeded($login)) {
-      $data['show_captcha'] = TRUE;
-      if ($data['use_recaptcha']) {
-        $data['recaptcha_html'] = $this->_create_recaptcha();
-      } else {
-        $data['captcha_html'] = $this->_create_captcha();
-      }
-      }
-      redirect("home");
     }
-  }
+    
+    function registration_success(){
+        $layout_data['top_bar'] = $this->load->view('layouts/logged_out_topbar','', true);
+        $layout_data['content_body'] = $this->load->view('auth/registration_success','', true);
+        $layout_data['footer'] = $this->load->view('layouts/footer', '', true);          
+        $this->load->view('layouts/main', $layout_data);
+    }
+    
+    function email_success(){
+        $layout_data['top_bar'] = $this->load->view('layouts/logged_out_topbar','', true);
+        $layout_data['content_body'] = $this->load->view('auth/email_conformation_success', '', true);
+        $layout_data['footer'] = $this->load->view('layouts/footer', '', true);          
+        $this->load->view('layouts/main', $layout_data);
+    }
+    
+    function login_fail(){
+        $layout_data['top_bar'] = $this->load->view('layouts/logged_out_topbar','', true);
+        $layout_data['content_body'] = $this->load->view('auth/login_fail', '', true);
+        $layout_data['footer'] = $this->load->view('layouts/footer', '', true);          
+        $this->load->view('layouts/main', $layout_data);
+    }
+    
+    function login()
+    {
+        if ($this->tank_auth->is_logged_in()) {                 // logged in
+            redirect('/home');
+        }elseif ($this->tank_auth->is_logged_in(FALSE)) {            // logged in, not activated
+          redirect('/auth/send_again/');
+        }   else {
+            $data['login_by_username'] = ($this->config->item('login_by_username', 'tank_auth') AND
+              $this->config->item('use_username', 'tank_auth'));
+            $data['login_by_email'] = $this->config->item('login_by_email', 'tank_auth');
+            
+            $this->form_validation->set_rules('login', 'Login', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('remember', 'Remember me', 'integer');
+            
+            // Get login for counting attempts to login
+            // if ($this->config->item('login_count_attempts', 'tank_auth') AND
+            //   ($login = $this->input->post('login'))) {
+            // $login = $this->security->xss_clean($login);
+            // } else {
+            //   //$login = '';
+            // }
+            
+            $login = $this->input->post('login');
+            $login = $this->security->xss_clean($login);
+            
+            $data['use_recaptcha'] = $this->config->item('use_recaptcha', 'tank_auth');
+            if ($this->tank_auth->is_max_login_attempts_exceeded($login)) {
+                if ($data['use_recaptcha']){
+                    //$this->form_validation->set_rules('recaptcha_response_field', 'Confirmation Code', 'trim|xss_clean|required|callback__check_recaptcha');
+                } else {
+                    //$this->form_validation->set_rules('captcha', 'Confirmation Code', 'trim|xss_clean|required|callback__check_captcha');
+                }
+            }
+            $data['errors'] = array();
+            
+            if ($this->form_validation->run()) {  // validation ok
+                $login_result = $this->tank_auth->login($this->form_validation->set_value('login'),
+                                                $this->form_validation->set_value('password'),
+                                                $this->form_validation->set_value('remember'),
+                                                $data['login_by_username'],
+                                                $data['login_by_email']);
+
+                if ($login_result) {               // success
+                    redirect('profile');
+                } else {
+                    $errors = $this->tank_auth->get_error_message();
+                    if (isset($errors['banned'])) {               // banned user
+                        $this->_show_message($this->lang->line('auth_message_banned').' '.$errors['banned']);
+                    } elseif (isset($errors['not_activated'])) {        // not activated user
+                        redirect('/auth/send_again/');
+                    } else {        // fail
+                        foreach ($errors as $k => $v) $data['errors'][$k] = $this->lang->line($v);
+                        redirect('/auth/login_fail', $data);
+                    }
+                }
+            }
+            $data['show_captcha'] = FALSE;
+            if ($this->tank_auth->is_max_login_attempts_exceeded($login)) {
+                $data['show_captcha'] = TRUE;
+                if ($data['use_recaptcha']) {
+                    $data['recaptcha_html'] = $this->_create_recaptcha();
+                } else {
+                    $data['captcha_html'] = $this->_create_captcha();
+                }
+            }
+            redirect("home");
+        }
+    }
 
 
   /**
