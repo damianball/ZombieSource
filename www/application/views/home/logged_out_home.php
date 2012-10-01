f09426a4dc70b98f88b1e8c4c966a11f9b0e4c30
Chandler Abraham <cabraham@twitter.com>
2012-10-01 00:09:47 -0700

    turning on registration

--- www/application/views/home/logged_out_home.php
+++ www/application/views/home/logged_out_home.php
@@ -1,6 +1,5 @@
-<!--  // @TODO: implement close registration
- -->
-<!--                 <h2> New here? </h2>
+
+                <h2> New here? </h2>
                 <div class="clearfix">
 
                     <a href=" <?php echo site_url("auth/register"); ?> " id = "test" class = "pull-left inline btn btn-large btn-primary"> 
@@ -10,7 +9,7 @@
                    </div>
 
 
- -->
+
      
       
      
