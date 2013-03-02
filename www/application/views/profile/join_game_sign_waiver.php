7cc323f86014acef39c4bd981c13d3cdb35506b0
Chandler Abraham <cabraham@twitter.com>
2013-03-02 02:49:28 -0800

    git rid of idaho waiver

--- www/application/views/profile/join_game_sign_waiver.php
+++ www/application/views/profile/join_game_sign_waiver.php
@@ -1,4 +1,4 @@
-  <div class = "join_game_sign_waiver">
+<!--   <div class = "join_game_sign_waiver">
     <h2>Sign Safety Waiver (Required) </h2>
     <a target="_blank" href= "https://docs.google.com/spreadsheet/viewform?formkey=dDVtb1dSa2c5aXB2SzJ2Rm8wYWduSVE6MQ">
       University of Idaho Online Waiver (Information is for University purposes only.)
@@ -8,4 +8,4 @@
           <input type="checkbox" name="waiversigned" value="1"> Check to indicate you filled out and understand the safety waiver.
       </label>
     </div>
-  </div>
+  </div> -->
